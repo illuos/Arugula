7c07edeb4fd3306d50f95616d194e2bf615885f4
Vins Sharma <vms5391@psu.edu>
2019-09-15 22:55:40 -0400

    initial bot setup

---
+++ index.js
@@ -0,0 +1,48 @@
+// * Imports
+const commando = require("discord.js-commando");
+const config = require("./config.json");
+
+// * Client Initialization
+const client = new commando.Client({
+    owner: config.owner,
+    commandPrefix: config.prefix
+});
+
+// * Core Event Handling
+client.on("ready", () => { // Startup
+    console.log(`Arugula is online on ${client.guilds.size} guilds.`);
+    client.user.setActivity("the plants grow | aru", {
+        type: "WATCHING"
+    });
+});
+client.on("message", () => {
+    // TODO: Add in more message handling
+});
+
+// * Guild Events
+client.on("guildCreate", guild => { // Joined Guild
+    console.log(`Arugula has joined ${guild.name}. ID: ${guild.id}`);
+});
+client.on("guildDelete", guild => { // Left Guild
+    console.log(`Arugula has left ${guild.name}. ID: ${guild.id}`);
+});
+
+// * Error Handling
+client.on("error", (e) => console.error(e));
+client.on("warn", (e) => console.warn(e));
+// ! For use in testing alone
+//// client.on("debug", (e) => console.info(e));
+
+// * Command Registry
+client.registry
+    .registerDefaultTypes()
+    .registerGroups([
+        ["basic", "Basic commands"],
+        ["owner", "Owner-only commands"]
+    ])
+    .registerDefaultGroups()
+    .registerDefaultCommands()
+    .registerCommandsIn(`${__dirname}/commands`);
+
+// * Magic!
+client.login(config.token);
