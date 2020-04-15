1fc406586b05ed3ccfb733899f5f0dc4cf98f896
Vins <sharmavins23@gmail.com>
2020-04-15 12:56:19 -0400

    adding server status command

---
+++ commands/basic/pingmc.js
@@ -0,0 +1,31 @@
+const commando = require("discord.js-commando");
+const { getStatus } = require("mc-server-status");
+
+class pingmc extends commando.Command {
+    constructor(client) {
+        super(client, {
+            name: "pingmc",
+            aliases: ["mc", "server", "pingserver"],
+            group: "basic",
+            memberName: "pingmc",
+            description: "Arugula pings the minecraft server and returns some information on its status.",
+            details: "Arugula pings the minecraft server, returning the version, player count, and ping.",
+            examples: ["aru! pingmc", "aru! mc", "aru! server", "aru! pingserver"],
+            args: [{
+                key: "server",
+                prompt: "What server would you like to ping?",
+                type: "string",
+                default: "andyusesrange.duckdns.org:25565"
+            }]
+        });
+    }
+    async run(message, args) {
+        let status = await getStatus(args.server);
+
+        message.react('✅');
+
+        message.channel.send(`\`\`\`${status}\n\`\`\``);
+    }
+}
+
+module.exports = pingmc;
